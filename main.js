79c89e2cca2e541e7c3ef783f3391298a4ebfcec
diegoBerlanga <jdruizberlanga@gmail.com>
2025-01-01 20:41:37 +0900

    1) Restructuration of some files (name changes, elements redistribution) 2) Testing code to check if the API is working

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-class MovieManager {
+const movieApiUrl = 'http://www.omdbapi.com/?i=tt3896198&apikey=f40b039d';
+const newElementFromAPI = document.getElementById('apiElement');
 
-}
+fetch(movieApiUrl).then(response => {
+    if(!response.ok) {
+        throw new Error('The API call failed');
+    }
 
-class Movie {
-
-}
+    return response.json();
+})
+.then(data => {
+    newElementFromAPI.textContent = JSON.stringify(data, null, 2);
+})
+.catch(error => {
+    console.log(error);
+});
